cd31c8407de58e9c2bdc300e56f27f0ae4316da3
justinbather <justinbather@hotmail.com>
2023-12-07 17:44:48 -0500

    add comments

--- day3/main.go
+++ day3/main.go
@@ -33,7 +33,6 @@ var specChars = []specChar{}
 // Parse through text
 // Find all numbers, saving the start and end index, line number
 // Find all special chars, saving the index, and line number
-// Loop over the numbers map
 
 func parseLine(line string, lineNum int) {
 
@@ -65,13 +64,6 @@ func parseLine(line string, lineNum int) {
 
 			}
 		}
-
-		// if char is a number
-		// while char is number
-		// add byte to number
-		// else if char is spec char
-		// create part
-
 	}
 }
 
@@ -95,13 +87,8 @@ func main() {
 
 	}
 
+	// Keeps track of the product sum
 	var sum int
-
-	type adj struct {
-		numAdj int
-		vals   []int
-		symbol specChar
-	}
 
 	type adjPair struct {
 		part    part
@@ -109,32 +96,32 @@ func main() {
 		matched bool
 	}
 
+	//Loop through, adding pairs of valid specChars and part numbers to a slice
+	//if char is on same line, below, or above the part number, and is next to or between partnumber
 	adjPairs := []adjPair{}
 	for _, part := range partsFound {
+
 		for _, specChar := range specChars {
-			fmt.Println("comparing ", part, specChar)
+
 			if (part.Line == (specChar.Line+1) || part.Line == (specChar.Line-1) || part.Line == specChar.Line) &&
 				((specChar.Index+1) >= part.StartIdx && (specChar.Index-1) <= part.EndIdx) {
-				fmt.Println(part)
 
 				adjPairs = append(adjPairs, adjPair{part: part, char: specChar, matched: false})
-
 			}
 		}
 	}
 
+	//Loop through and compare pairs together, getting the product of 2 part numbers if they share the same specChar
+	//Set matched to true so we dont double up on the matching
 	for j, outerPair := range adjPairs {
 
 		for _, innerPair := range adjPairs[j+1:] {
 			if (innerPair.char == outerPair.char) && (innerPair.matched == false) && (outerPair.matched == false) {
 				sum += (innerPair.part.Value * outerPair.part.Value)
-				fmt.Println("Matched: ", outerPair.part.Value, innerPair.part.Value)
 				innerPair.matched = true
 			}
 		}
 	}
-
-	fmt.Println(specChars)
 
 	fmt.Println(sum)
 	if err := scanner.Err(); err != nil {
