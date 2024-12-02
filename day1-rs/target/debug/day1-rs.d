6e61d70ecf5e1dd10acf46fa77e023b4cb10e97b
Justin Bather <jbather@apexfintechsolutions.com>
2024-12-02 08:46:43 +0000

    remove unused

--- day1-rs/target/debug/day1-rs.d
+++
@@ -1,1 +0,0 @@
-/Users/justinbather/Desktop/Projects/advent-of-code/day1-rs/target/debug/day1-rs: /Users/justinbather/Desktop/Projects/advent-of-code/day1-rs/src/main.rs
--- day1-rs/target/debug/deps/day1_rs-25927d91cd3491d6.d
+++
@@ -1,5 +0,0 @@
-/Users/justinbather/Desktop/Projects/advent-of-code/day1-rs/target/debug/deps/day1_rs-25927d91cd3491d6: src/main.rs
-
-/Users/justinbather/Desktop/Projects/advent-of-code/day1-rs/target/debug/deps/day1_rs-25927d91cd3491d6.d: src/main.rs
-
-src/main.rs:
