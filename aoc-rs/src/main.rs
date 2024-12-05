625b535d4576297173760ead80fca35a94cc4d6b
Justin Bather <jbather@apexfintechsolutions.com>
2024-12-05 17:01:03 +0000

    setup day5

--- aoc-rs/src/main.rs
+++ aoc-rs/src/main.rs
@@ -4,13 +4,15 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     //day1::run();
     //day2::run();
     //day3::run();
+    //day4::run();
     //let now = Instant::now();
-    day4::run();
+    day5::run();
     //let elapsed = now.elapsed();
     //println!("Elapsed: {:.2?}", elapsed);
 }
