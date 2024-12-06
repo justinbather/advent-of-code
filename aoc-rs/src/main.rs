6281687a78b9e69fe74cc0fa8946ff45c0aa9367
justinbather <justinbather@hotmail.com>
2024-12-06 15:24:36 +0000

    day6 p1

--- aoc-rs/src/main.rs
+++ aoc-rs/src/main.rs
@@ -5,14 +5,16 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     //day1::run();
     //day2::run();
     //day3::run();
     //day4::run();
-    let now = Instant::now();
-    day5::run();
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    //day5::run();
+    day6::run();
+    //let now = Instant::now();
+    //let elapsed = now.elapsed();
+    //println!("Elapsed: {:.2?}", elapsed);
 }
