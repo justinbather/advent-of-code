18228630d2f46c52aca8a29681963ab4636d508d
justinbather <justinbather@hotmail.com>
2024-12-07 12:51:00 +0000

    brute force day 7 part 2

--- 2024/src/day7/mod.rs
+++ 2024/src/day7/mod.rs
@@ -9,40 +9,91 @@ pub fn run() {
 
     let calibrations = parse_calibrations(reader);
 
-    let calibration_results = part1(calibrations);
-    println!("Total calibration result is: {calibration_results}")
+    let part1_results = part1(&calibrations);
+    println!("Total part1 calibration result is: {part1_results}");
+
+    let part2_results = part2(&calibrations);
+    println!("Total part2 calibration result is: {part2_results}");
 }
-fn part1(calibrations: Vec<Calibration>) -> i64 {
-    let mut num_valid = 0;
+
+fn part2(calibrations: &Vec<Calibration>) -> i64 {
+    let mut test_results = 0;
     for c in calibrations.iter() {
-        let results = compute_combinations(&c.numbers);
+        let results = compute_combinations_with_pipe(&c.numbers);
         if results.contains(&c.test_val) {
-            num_valid += c.test_val;
+            test_results += c.test_val;
         }
     }
 
-    num_valid
+    test_results
 }
 
-fn compute_combinations(numbers: &Vec<i64>) -> HashSet<i64> {
-    fn helper(index: usize, current_value: i64, numbers: &Vec<i64>, results: &mut HashSet<i64>) {
-        // If we've processed all numbers, store the current result
+fn part1(calibrations: &Vec<Calibration>) -> i64 {
+    let mut test_results = 0;
+    for c in calibrations.iter() {
+        let results = compute_combinations(&c.numbers);
+        if results.contains(&c.test_val) {
+            test_results += c.test_val;
+        }
+    }
+
+    test_results
+}
+
+fn compute_combinations_with_pipe(numbers: &Vec<i64>) -> HashSet<i64> {
+    fn recurse(index: usize, current_value: i64, numbers: &Vec<i64>, results: &mut HashSet<i64>) {
         if index == numbers.len() {
             results.insert(current_value);
             return;
         }
 
-        helper(index + 1, current_value + numbers[index], numbers, results);
+        recurse(index + 1, current_value + numbers[index], numbers, results);
 
-        helper(index + 1, current_value * numbers[index], numbers, results);
+        recurse(index + 1, current_value * numbers[index], numbers, results);
+
+        recurse(
+            index + 1,
+            pipe(current_value, numbers[index]),
+            numbers,
+            results,
+        );
     }
 
     let mut results = HashSet::new();
     if !numbers.is_empty() {
-        helper(1, numbers[0], numbers, &mut results); // Start recursion from the first number
+        recurse(1, numbers[0], numbers, &mut results);
     }
 
     results
+}
+
+fn compute_combinations(numbers: &Vec<i64>) -> HashSet<i64> {
+    fn recurse(index: usize, current_value: i64, numbers: &Vec<i64>, results: &mut HashSet<i64>) {
+        if index == numbers.len() {
+            results.insert(current_value);
+            return;
+        }
+
+        recurse(index + 1, current_value + numbers[index], numbers, results);
+
+        recurse(index + 1, current_value * numbers[index], numbers, results);
+    }
+
+    let mut results = HashSet::new();
+    if !numbers.is_empty() {
+        recurse(1, numbers[0], numbers, &mut results);
+    }
+
+    results
+}
+
+fn pipe(left: i64, right: i64) -> i64 {
+    let left = left.to_string();
+    let right = right.to_string();
+
+    let new = String::from(left + &right);
+
+    new.parse().unwrap()
 }
 
 fn parse_calibrations(reader: BufReader<File>) -> Vec<Calibration> {
