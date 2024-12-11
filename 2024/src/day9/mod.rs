9bad69d44e58a6304ce4afe64b5db2ded8a31ee3
Justin Bather <jbather@apexfintechsolutions.com>
2024-12-11 12:16:41 +0000

    finished day9 part 2

--- 2024/src/day9/mod.rs
+++ 2024/src/day9/mod.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use std::{
+    ffi::c_ushort,
     fs::File,
     io::{BufReader, Read},
     iter::StepBy,
@@ -7,13 +8,13 @@ use std::{
 
 #[allow(unused)]
 pub fn run() {
-    let mut reader = aoc::read_file("src/day9/input-test.txt");
+    let mut reader = aoc::read_file("src/day9/input.txt");
     let diskmap = read_into_diskmap(&mut reader);
 
     let _p1_checksum = part1(&diskmap);
     // println!("Checksum: {p1_checksum}");
 
-    println!("Diskmap:\n{:?}", diskmap);
+    //println!("Diskmap:\n{:?}", diskmap);
     let p2_checksum = part2(&diskmap);
     println!("Checksum p2: {p2_checksum}")
 }
@@ -26,26 +27,21 @@ fn part1(diskmap: &Vec<i32>) -> u64 {
     // n == size of the file block
     let mut blocks = diskmap_to_optionals(&diskmap);
 
-    compress1(&mut blocks);
+    compress_with_fragmentation(&mut blocks);
 
-    let checksum = get_checksum(blocks);
-    checksum
+    get_checksum(blocks)
 }
 
 fn part2(diskmap: &Vec<i32>) -> u64 {
+    // using the block structure so we know the contiguous size of a file or space,
     let mut blocks = diskmap_to_blocks(&diskmap);
-    println!("Blocks: ");
-    println!("{:?}", blocks);
 
-    let blocks = compress2(&mut blocks);
-    let mut s = String::new();
-    for block in blocks.iter() {
-        s += &block.to_string();
-    }
+    compress(&mut blocks);
 
-    println!("Compresed Blocks: {s}");
-    println!("{:?}", blocks);
-    0
+    // convert back to optionals, so we have can have multi digit ids, only taking up one index
+    let res = blocks_to_optional(blocks);
+
+    get_checksum(res)
 }
 
 fn get_checksum(blocks: Vec<Option<i32>>) -> u64 {
@@ -60,48 +56,109 @@ fn get_checksum(blocks: Vec<Option<i32>>) -> u64 {
     checksum
 }
 
-// Sorts (compresses) a block vector, in place
-fn compress2(blocks: &mut Vec<Block>) -> Vec<Block> {
-    // start from end, then look for the first space itll fit
-
-    let mut new_blocks: Vec<Block> = blocks.clone();
-
-    //iterate backwards
-    for (idx, block) in blocks.iter().rev().enumerate() {
+fn get_checksum_blocks(blocks: Vec<Block>) -> u64 {
+    let mut checksum: u64 = 0;
+    for (idx, block) in blocks.iter().enumerate() {
         match block {
-            Block::File(outer_id, outer_size) => {
-                for (inner_idx, inner_block) in blocks.iter().enumerate() {
-                    match inner_block {
-                        Block::File(_, _) => continue,
-                        Block::Empty(size) => {
-                            if size >= outer_size {
-                                //we can swap these two
-                                // if they are equal we just swap
-                                // if size is bigger than file, we need to get the difference and
-                                // insert it after the file
+            Block::File(id, _) => {
+                checksum += *id as u64 * idx as u64;
+            }
+            Block::Empty(_) => {}
+        }
+    }
 
-                                if size == outer_size {
-                                    //clean swap
-                                    new_blocks.swap(idx, inner_idx);
-                                } else {
-                                    // space is bigger than file
-                                    let remainder = size - outer_size;
-                                    new_blocks[inner_idx] = *block;
-                                    new_blocks.insert(inner_idx + 1, Block::Empty(remainder));
-                                }
-                            }
-                        }
+    checksum
+}
+
+fn blocks_to_optional(blocks: Vec<Block>) -> Vec<Option<i32>> {
+    let mut result: Vec<Option<i32>> = Vec::new();
+    for (idx, block) in blocks.iter().enumerate() {
+        match block {
+            Block::File(id, size) => {
+                for _ in 0..*size {
+                    result.push(Some(*id));
+                }
+            }
+            Block::Empty(size) => {
+                if *size == 0 {
+                    continue;
+                } else {
+                    for _ in 0..*size {
+                        result.push(None);
                     }
                 }
             }
-            Block::Empty(_) => continue,
         }
     }
-    new_blocks
+
+    result
 }
 
-// Sorts (compresses) a block vector, in place
-fn compress1(blocks: &mut Vec<Option<i32>>) {
+// Sorts (compresses) a block vector, in place, keeping contiguous memory in tact
+fn compress(blocks: &mut Vec<Block>) {
+    // starting from the end, find the first block we need to move (farthest right)
+    let mut far = blocks.len() - 1;
+    while far > 0 {
+        let mut file_size = 0;
+        match blocks[far] {
+            Block::File(_, size) => {
+                file_size = size;
+            }
+            Block::Empty(_) => {
+                far -= 1;
+                continue;
+            }
+        }
+
+        // we have the file that needs to be moved
+        // loop through blocks to find the first spot it can be moved to
+
+        // stay in bounds between start of blocks and the far pointer ie where the file is
+        let mut cursor = 0;
+        while cursor < far {
+            match blocks[cursor] {
+                Block::File(_, _) => {
+                    cursor += 1;
+                    continue;
+                }
+                Block::Empty(empty_chunk_size) => {
+                    // cant fit here
+                    if empty_chunk_size < file_size {
+                        cursor += 1;
+                        continue;
+                    } else {
+                        let remainder = empty_chunk_size - file_size;
+                        if remainder == 0 {
+                            blocks.swap(cursor, far);
+                        } else {
+                            // move replaced memory to the tail, add left over memory behind the
+                            // file chunk
+                            let remaining_chunk = Block::Empty(remainder);
+                            let replaced_chunk = Block::Empty(empty_chunk_size - remainder);
+                            // set current chunk to new size
+                            blocks[cursor] = replaced_chunk;
+                            // swap resized chunk with the file
+                            blocks.swap(cursor, far);
+                            // add the remaining empty chunk behind the file
+                            blocks.insert(cursor + 1, remaining_chunk);
+
+                            // normalize the far pointers position by making up for the increase in
+                            // length, else we could skip a movable file
+                            far += 1;
+                        }
+                        //trigger while loop to end
+                        cursor = far;
+                    }
+                }
+            }
+        }
+        far -= 1;
+    }
+}
+
+// Sorts (compresses) a block vector, in place, with no concern about fragmenting (not keeping
+// related data next to each other)
+fn compress_with_fragmentation(blocks: &mut Vec<Option<i32>>) {
     let mut close: usize = 0;
     let mut far: usize = blocks.len() - 1;
 
@@ -195,7 +252,7 @@ fn read_into_diskmap(reader: &mut BufReader<File>) -> Vec<i32> {
 }
 
 //A block can either have a file or represent empty space
-//Block::File(i32, usize) will contain the file ID, and its size
+//Block::File(i32, i32) will contain the file ID, and its size
 //Block::Empty(usize) will contain its size
 #[derive(Debug, Clone, Copy)]
 enum Block {
